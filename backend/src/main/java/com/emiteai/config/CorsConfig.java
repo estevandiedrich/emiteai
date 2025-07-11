2feb988826748f03dbd505b8331ee257ce34a631
estevandiedrich <estevan.diedrich@gmail.com>
2025-07-11 11:46:33 -0300

    cors adjustment

--- backend/src/main/java/com/emiteai/config/CorsConfig.java
+++ backend/src/main/java/com/emiteai/config/CorsConfig.java
@@ -7,7 +7,7 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@Profile("!dev") // Ativado para todos os profiles EXCETO dev
+@Profile("disabled") // Desabilitado - usando configuração no AuditoriaConfig
 public class CorsConfig implements WebMvcConfigurer {
 
     @Override
